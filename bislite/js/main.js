282838eb4313c79295659c94dbec2894ea35ae76
misha-mozart <mas-motor@rambler.ru>
2021-05-05 00:33:52 +0300

    added start project

---
+++ bislite/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $('.slider__inner').slick({
+        arrows:false,
+        dots:true,
+    });
+
+    $('.works__inner').slick({
+        slidesToShow:4,
+        slidesToScroll:1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/slide-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/slide-right.svg" alt=""></button>',
+    });
+
+  });
